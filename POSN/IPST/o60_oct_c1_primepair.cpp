e7d64002c39763081fa703d15385b4e71f793fd6
Nonbangkok <nonbangkokgamer@gmail.com>
2025-01-27 21:41:21 +0700

    27012025

---
+++ POSN/IPST/o60_oct_c1_primepair.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define coutf(n, m) cout << fixed << setprecision(n) << m
+#define forr(i, a, n) for (int i = a; i < n; i++)
+#define forl(i, a, n) for (int i = a; i > n; i--)
+#define macos ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define endll "\n"
+#define sp " "
+typedef long long ll;
+using namespace std;
+
+const int N = 1e6+10;
+int dp[N],a,b,c,d;
+
+int main(){macos;
+
+    cin >> a >> b >> c >> d;
+    forr(i,2,N){
+        for(int j=0;j<N;j+=i)dp[i]
+    }
+
+    return 0;
+}
