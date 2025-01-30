bd690b4747001f2554b7e9137562e54c7ba0d26d
Nonbangkok <nonbangkokgamer@gmail.com>
2025-01-30 19:51:56 +0700

    30012025

--- POSN/TOI/TOI10_Crazy_Admin.cpp
+++ POSN/TOI/TOI10_Crazy_Admin.cpp
@@ -2,37 +2,35 @@
 #define coutf(n, m) cout << fixed << setprecision(n) << m
 #define forr(i, a, n) for (int i = a; i < n; i++)
 #define forl(i, a, n) for (int i = a; i > n; i--)
-#define macos ios::sync_with_stdio(0);cin.tie(0)
+#define macos ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 #define endll "\n"
 #define sp " "
 typedef long long ll;
 using namespace std;
-int main() {macos;
 
-    int k,n,mx=-1;
-    cin >> k >> n;
-    int A[n];
-    forr(i,0,n){
-        cin >> A[i];
-        mx=max(mx,A[i]);
-    }
+const int N = 100;
+int A[N];
+int n,m,mx;
 
-    int l=mx,r=1e7,mid;
-    while(l<=r){
-        mid=(l+r)/2;
-        int sum=0,cnt=0;
-        forr(i,0,n){
-            if(sum+A[i]<=mid){
-                sum+=A[i];
-            }else{
-                sum=A[i];
-                cnt++;
-            }
+int main(){macos;
+
+    cin >> n >> m;
+    forr(i,0,m)cin >> A[i],mx=max(mx,A[i]);
+
+    int l = mx,r = 1e7,mid;
+    while(l<r){
+        mid = (l+r) >> 1;
+
+        int cnt = 0,kps = 0;
+        forr(i,0,m){
+            if(kps+A[i]<=mid)kps += A[i];
+            else kps = A[i],cnt++;
         }
-        if(cnt>=k) l=mid+1;
-        else r=mid-1;
+
+        if(cnt>=n)l = mid+1;
+        else r = mid;
     }
-    cout << r+1;
+    cout << l;
 
     return 0;
 }
