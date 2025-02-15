d9a741262c8803ee3d926288358b6844dc6e6506
Nonbangkok <nonbangkokgamer@gmail.com>
2025-02-15 09:11:59 +0700

    15022025

--- POSN/TOI/TOI08_electricity.cpp
+++ POSN/TOI/TOI08_electricity.cpp
@@ -2,29 +2,32 @@
 #define coutf(n, m) cout << fixed << setprecision(n) << m
 #define forr(i, a, n) for (int i = a; i < n; i++)
 #define forl(i, a, n) for (int i = a; i > n; i--)
-#define macos ios::sync_with_stdio(0);cin.tie(0)
+#define macos ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 #define endll "\n"
 #define sp " "
 typedef long long ll;
 using namespace std;
-int main() {macos;
 
-    int n,k;
+const int N = 5e5+10;
+int dp[N],A[N];
+int n,k;
+deque<pair<int,int>> dq;
+
+int main(){macos;
+
     cin >> n >> k;
-    int A[n],dp[n];
     forr(i,0,n)cin >> A[i];
 
-    deque<pair<int,int>> q;
-    q.push_back({A[0],0});
-    dp[0]=A[0];
+    dq.push_back({0,A[0]});
 
     forr(i,1,n){
-        while(!q.empty()&&q.front().second<i-k)q.pop_front();
-        dp[i]=A[i]+q.front().first;
-        while(!q.empty()&&q.back().first>dp[i])q.pop_back();
-        q.push_back({dp[i],i});
+        while(!dq.empty()&&i-dq.front().first>k)dq.pop_front();
+        dp[i] = dq.front().second + A[i];
+        while(!dq.empty()&&dp[i]<=dq.back().second)dq.pop_back();
+        dq.push_back({i,dp[i]});
     }
+
     cout << dp[n-1];
 
     return 0;
- }
+}
