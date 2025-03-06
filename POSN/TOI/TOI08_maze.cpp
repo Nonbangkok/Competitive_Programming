b463dacbffc326b503ae93787fbac25fb342cb56
Nonbangkok <nonbangkokgamer@gmail.com>
2025-03-06 14:16:05 +0700

    06032025

--- POSN/TOI/TOI08_maze.cpp
+++ POSN/TOI/TOI08_maze.cpp
@@ -2,92 +2,49 @@
 #define coutf(n, m) cout << fixed << setprecision(n) << m
 #define forr(i, a, n) for (int i = a; i < n; i++)
 #define forl(i, a, n) for (int i = a; i > n; i--)
-#define macos ios::sync_with_stdio(0);cin.tie(0)
+#define macos ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 #define endll "\n"
 #define sp " "
 typedef long long ll;
 using namespace std;
 
-struct Non{
-    int ix,jx;
-};
+const int N = 160;
+int n,m,mn=1e9,cnt;
+int src[2],des[2],dis[2][N][N],A[N][N];
+int di[] = {0,0,-1,1};
+int dj[] = {-1,1,0,0};
+queue<tuple<int,int,int>> q;
 
-const int N=150;
-int A[N][N];
-int visited[N][N][2];
-int n,m;
-int di[]={1,-1,0,0};
-int dj[]={0,0,-1,1};
-
-void trvsal(int i,int j,int state){
-    int lvl=1;
-    queue<Non> q;
-    q.push({i,j});
-    visited[i][j][state]=1;
-
+void bfs(int x, int y, int s){
+    q.push({x,y,dis[s][x][y]=1});
     while(!q.empty()){
-        int cntlvl=q.size();
-        lvl++;
-        while(cntlvl--){
-            i = q.front().ix;
-            j = q.front().jx;
-            q.pop();
+        auto [i,j,c] = q.front();
+        q.pop();
 
-            forr(k,0,4){
-                int ik=i+di[k];
-                int jk=j+dj[k];
-                if(ik<0||jk<0||ik>=n||jk>=m) continue;
-                if(visited[ik][jk][state]) continue;
-                if(!A[ik][jk]){
-                    visited[ik][jk][state]=lvl;
-                    continue;
-                }
-                q.push({ik,jk});
-                visited[ik][jk][state]=lvl;
-            }
+        forr(k,0,4){
+            int ik = i+di[k];
+            int jk = j+dj[k];
+
+            if(ik<1||ik>n||jk<1||jk>m||dis[s][ik][jk])continue;
+            dis[s][ik][jk] = c + 1;
+            if(!A[ik][jk])continue;
+            q.push({ik,jk,c+1});
         }
     }
 }
 
-int main() {macos;
+int main(){macos;
 
-    int sti,stj,endi,endj;
-    cin >> n >> m >> sti >> stj >> endi >> endj;
+    cin >> n >> m >> src[0] >> src[1] >> des[0] >> des[1];
+    forr(i,1,n+1)forr(j,1,m+1)cin >> A[i][j];
 
-    forr(i,0,n){
-        forr(j,0,m){
-            cin >> A[i][j];
-        }
-    }
-    trvsal(sti-1,stj-1,0);
-    trvsal(endi-1,endj-1,1);
+    bfs(src[0],src[1],0);
+    bfs(des[0],des[1],1);
 
-    // cout << endll;
-    // forr(i,0,n){
-    //     forr(j,0,m){
-    //         cout << visited[i][j][0] << sp;
-    //     }
-    //     cout << endll;
-    // }
-    // cout << endll;
-    // forr(i,0,n){
-    //     forr(j,0,m){
-    //         cout << visited[i][j][1] << sp;
-    //     }
-    //     cout << endll;
-    // }
+    forr(i,1,n+1)forr(j,1,m+1)if(!A[i][j]&&dis[0][i][j]&&dis[1][i][j])
+        cnt++,mn = min(mn,dis[0][i][j]+dis[1][i][j]-1);
 
-    int cntans=0,mn=INT_MAX;
-    forr(i,0,n){
-        forr(j,0,m){
-            if(!A[i][j]&&visited[i][j][0]&&visited[i][j][1]){
-                cntans++;
-                mn=min(mn,visited[i][j][0]+visited[i][j][1]-1);
-            }
-        }
-    }
-
-    cout << cntans << endll << mn;
+    cout << cnt << endll << mn;
 
     return 0;
 }
