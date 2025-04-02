bfe0fe1eeb137b3c97cbb1307db1c3e7b2d060ca
Nonbangkok <nonbangkokgamer@gmail.com>
2025-04-02 10:25:06 +0700

    02042025

--- POSN/TOI/TOI19_Merge.cpp
+++ POSN/TOI/TOI19_Merge.cpp
@@ -7,27 +7,28 @@
 #define sp " "
 typedef long long ll;
 using namespace std;
-const int N=1e5+10;
-int x[N],s[N],y[N],t[N];
+
+const int N = 1e5 + 10;
+ll n,m,q,a,b,k;
+ll X[N],Y[N],S[N],T[N];
+
 int main(){macos;
 
-    int n,m,q,a,b,k;
     cin >> n >> m >> q;
-
-    forr(i,0,n)cin >> x[i];
-    forr(i,1,n+1){cin >> s[i];s[i]+=s[i-1];}
-    forr(i,0,m)cin >> y[i];
-    forr(i,1,m+1){cin >> t[i];t[i]+=t[i-1];}
+    forr(i,1,n+1)cin >> X[i];
+    forr(i,1,n+1)cin >> S[i],S[i] += S[i-1];
+    forr(i,1,m+1)cin >> Y[i];
+    forr(i,1,m+1)cin >> T[i],T[i] += T[i-1];
 
     while(q--){
         cin >> a >> b >> k;
-        int l=INT_MIN,r=INT_MAX,mid;
+        ll l = -1e9 - 10, r = 1e9 + 10, mid;
         while(l<r){
-            mid=(l+r)>>1;
-            int i=upper_bound(x,x+n,mid)-x;
-            int j=upper_bound(y,y+m,(mid-b)/a)-y;
-            if(s[i]+t[j]>=k)r=mid;
-            else l=mid+1
+            mid = (l+r) >> 1;
+            int ix = upper_bound(X+1,X+n+1,mid)-X-1;
+            int iy = upper_bound(Y+1,Y+m+1,(mid-b)/a)-Y-1;
+            if(S[ix]+T[iy]<k)l = mid +1;
+            else r = mid;
         }
         cout << l << endll;
     }
